39f5cba420c154f27367e91567ec4bbd860dac05
Ishthiyaq <ishthiyaqahamed@gmail.com>
2026-09-17 16:11:18 +0530

    Bug fixing

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -4,6 +4,8 @@ import { prisma } from "@/lib/prisma";
 import { verifyAndConsumeOtp } from "@/lib/otp";
 import { Role } from "@prisma/client";
 
+export const dynamic = "force-dynamic";
+
 export async function POST(req: Request) {
   try {
     const body = await req.json();
